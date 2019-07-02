1db335221ac2fec3c58e5f58609e1b53b441c077
udgover <udgover@digital-forensic.org>
2019-07-02 15:22:16 +0200

    Remove single quote when downloading file

--- web/views/helpers.py
+++ web/views/helpers.py
@@ -95,7 +95,7 @@ def file_download(filepath):
     with open(filepath, 'rb') as fd:
         response = make_response(fd.read())
 
-    response.headers["Content-Disposition"] = u"attachment; filename='{0}'".format(basename(filepath)).encode('latin-1', errors='ignore')
+    response.headers["Content-Disposition"] = u"attachment; filename={0}".format(basename(filepath)).encode('latin-1', errors='ignore')
 
     return response
 
@@ -159,4 +159,3 @@ def comments_enabled():
         comments_enabled = config.get_values()['enable']
 
     return comments_enabled
-    
