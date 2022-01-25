2498a154cb9129737d399b1a4a98ecf52ebf3616
Gael Muller <muller.gael@gmail.com>
2022-01-25 16:54:45 +0100

    fix(single_module): fix test mode with mongodb authentication enabled

--- fame/core/store.py
+++ fame/core/store.py
@@ -1,12 +1,11 @@
 from urllib.parse import quote_plus
-from pymongo import MongoClient, TEXT
 
+from pymongo import TEXT, MongoClient
 
 from fame.common.config import fame_config
 
 
 class Store:
-
     def __init__(self):
         self.init()
 
@@ -14,9 +13,7 @@ class Store:
         # Connection
         self._con = MongoClient(fame_config.mongo_host, int(fame_config.mongo_port), serverSelectionTimeoutMS=10000)
         self.db = self._con[fame_config.mongo_db]
-        if fame_config.mongo_user and fame_config.mongo_password:
-            self.db.authenticate(fame_config.mongo_user, quote_plus(fame_config.mongo_password), mechanism='SCRAM-SHA-1')
-            
+
         # Collections
         self.files = self.db.files
         self.analysis = self.db.analysis
@@ -35,15 +32,22 @@ class Store:
     def connect(self):
         self.init()
 
+        # Authenticate
+        if fame_config.mongo_user and fame_config.mongo_password:
+            self.db.authenticate(
+                fame_config.mongo_user, quote_plus(fame_config.mongo_password), mechanism="SCRAM-SHA-1"
+            )
+
         # Create indexes
-        self.files.create_index('md5')
-        self.files.create_index('sha1')
-        self.files.create_index('sha256')
+        self.files.create_index("md5")
+        self.files.create_index("sha1")
+        self.files.create_index("sha256")
         self.files.create_index([("$**", TEXT)], background=True)
-        self.analysis.create_index('date')
+        self.analysis.create_index("date")
         self.analysis.create_index([("$**", TEXT)], background=True)
 
     def collection(self, name):
         return self.db[name]
+
 
 store = Store()
