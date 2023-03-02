19e6065f7c15eb3eab9072893b03069cb4add589
Augustin-FL <Augustin-FL@users.noreply.github.com>
2023-03-02 14:33:33 +0100

    Clean .pid & .sock files before running dockerd Close #131

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,7 @@ utils/run.sh "utils/install.py" "$1" "--not-interactive"
 if [ "$1" == "web" ]; then
   utils/run.sh webserver.py
 elif [ "$1" == "worker" ]; then
+  sudo rm -f /var/run/docker.sock /var/run/docker/containerd/containerd.sock /var/run/docker.pid /var/run/docker/containerd/containerd.pid
   sudo dockerd &> /dev/null &
   utils/run.sh worker.py unix
 elif [ "$1" == "updater" ]; then
